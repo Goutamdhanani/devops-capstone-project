dad35705a574df3f3254402f5ed82d961d9e8950
goutam <goutamdhanani@gmail.com>
2025-06-19 16:12:26 -0400

    Fixed flake8 lint errors

--- service/__init__.py
+++ service/__init__.py
@@ -20,19 +20,15 @@ app.config.from_object(config)
 from service import routes, models  # noqa: F401 E402
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
-
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
-
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
-
 try:
     models.init_db(app)  # make our database tables
 except Exception as error:  # pylint: disable=broad-except
     app.logger.critical("%s: Cannot continue", error)
     # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
-
 app.logger.info("Service initialized!")
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,21 +12,13 @@ from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
-
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
-
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
-
-######################################################################
-#  T E S T   C A S E S
-######################################################################
-
 class TestAccountService(TestCase):
     """Account Service Tests"""
-
     @classmethod
     def setUpClass(cls):
         """Run once before all tests"""
@@ -37,26 +29,17 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
-
     def setUp(self):
         """Runs before each test"""
         db.session.query(Account).delete()  # clean up the last tests
         db.session.commit()
-
         self.client = app.test_client()
-
     def tearDown(self):
         """Runs once after each test case"""
         db.session.remove()
-
-    ######################################################################
-    #  H E L P E R   M E T H O D S
-    ######################################################################
-
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -72,23 +55,16 @@ class TestAccountService(TestCase):
             account.id = new_account["id"]
             accounts.append(account)
         return accounts
-
-    ######################################################################
-    #  A C C O U N T   T E S T   C A S E S
-    ######################################################################
-
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
     def test_health(self):
         """It should be healthy"""
         resp = self.client.get("/health")
         self.assertEqual(resp.status_code, 200)
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
-
     def test_create_account(self):
         """It should Create a new Account"""
         account = AccountFactory()
@@ -98,11 +74,9 @@ class TestAccountService(TestCase):
             content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
         # Make sure location header is set
         location = response.headers.get("Location", None)
         self.assertIsNotNone(location)
-
         # Check the data is correct
         new_account = response.get_json()
         self.assertEqual(new_account["name"], account.name)
@@ -110,12 +84,10 @@ class TestAccountService(TestCase):
         self.assertEqual(new_account["address"], account.address)
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
-
     def test_bad_request(self):
         """It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
     def test_unsupported_media_type(self):
         """It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
@@ -125,7 +97,6 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
     def test_read_an_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
@@ -134,12 +105,10 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
         self.assertEqual(data["email"], account.email)
-
     def test_read_account_not_found(self):
         """It should return 404 when Account is not found"""
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
     def test_list_accounts(self):
         """It should List all Accounts"""
         self._create_accounts(3)
@@ -154,7 +123,6 @@ class TestAccountService(TestCase):
         updated_email = "updated@example.com"
         account.name = updated_name
         account.email = updated_email
-
         response = self.client.put(
             f"{BASE_URL}/{account.id}",
             json=account.serialize(),
@@ -164,8 +132,6 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(data["name"], updated_name)
         self.assertEqual(data["email"], updated_email)
-
-
     def test_update_account_not_found(self):
         """It should return 404 when updating non-existent Account"""
         fake_account = AccountFactory()
@@ -175,18 +141,15 @@ class TestAccountService(TestCase):
             content_type="application/json"
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         response = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
     def test_delete_account_not_found(self):
         """It should return 204 even if Account is not found (idempotent delete)"""
         response = self.client.delete(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -198,8 +161,7 @@ class TestAccountService(TestCase):
             'Referrer-Policy': 'strict-origin-when-cross-origin'
         }
         for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)
-        
+            self.assertEqual(response.headers.get(key), value)       
     def test_cors_headers(self):
         """It should return CORS headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
